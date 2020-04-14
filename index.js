e6afa1a036a72d809fab9a5f9b0a92083a6a9478
oussamahassani <hassani20120@gmail.com>
2020-04-14 15:51:10 +0100

    debut jeux

---
+++ index.js
@@ -0,0 +1,15 @@
+const cvs = document.getElementById("myCanvas");
+const ctx = cvs.getContext("2d");
+cvs.width = 500;
+cvs.height = 500;
+/*cvs.style.backgroundImage = "url('image/backround.jpeg')";*/
+
+// create box = 10-> en x ; 30->y ()  ( 50 case en x et  16 case en y)
+const boxx = 10;
+const boxy = 30
+// ajouter function main ;
+function main()
+{
+
+}
+let game = setInterval(main, 100);
