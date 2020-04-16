aad399f16de86b76c6e122d609b2b704f6add42d
oussamahassani <hassani20120@gmail.com>
2020-04-16 14:07:42 +0100

    ajoute sons et faire une repo pour les image

--- index.js
+++ index.js
@@ -1,55 +1,289 @@
-const cvs = document.getElementById('myCanvas');
-const ctx = cvs.getContext('2d');
-cvs.width = 600;
-cvs.height = 600;
 
-// create box = 10-> en x ; 30->y ()  ( 50 case en x et  16 case en y)
-const boxx = 10;
-const boxy = 30;
+const cvs = document.getElementById("myCanvas");
+cvs.style.display ="none";
+buttonstop = document.createElement("button");
+cvs.appendChild(buttonstop);
+const ctx = cvs.getContext("2d");
+var c = document.querySelector('#c');
+cvs.width = 760;
+cvs.height = 760;
+let inputscore = document.getElementById("score");
+/*cvs.style.backgroundImage = "url('image/backround.jpeg')";*/
+function commancer() {
+    if (document.getElementById("nom").value != 0)
+    {
+        document.getElementById("nomm").style = "display: none";
+        cvs.style.display="block";
 
- // ajouter des image food intiale et arriere plans
-const foodimg = new Image();
-let foodimage = ['banane.png', 'banane.png', 'banane.png'];
-let randam = Math.floor(Math.random() * foodimg.length);
-foodimg.src = foodimg[randam];
-const planimg = new Image();
-planimg.src = 'Background.png';
-// initialiser la valeur de score et la valeur de food et l'entete de snak
-let score = 0;
-let food = {
-	x: Math.floor(Math.random() * boxx * 58),
-	y: Math.floor(Math.random() * boxy * 18),
-};
-let snak = [];
-    snak[0] = {
-        x : boxx ,
-        y  : boxy
-}
-snakx = snak[0].x;
-snaky = snak[0].x;
-// ajouter function main ;
-function main() {
-  ctx.drawImage(planimg, 0, 0);
-  for( let i=0 ; i< snak.length;i++)
-  {
-  ctx.fillStyle = 'rgb(' + Math.floor( 255 -15 * i) + ', ' +    /* randam couleur snak  boucle for */ 
-  Math.floor(255 -15*2*i) + ', 0)';
-  ctx.fillRect(snak[0].x, snak[0].y, boxx, boxy);
-  ctx.strokeRect(snak[0].x, snak[0].y, boxx, boxy);      
-	if (snakx  == food.x && snaky == food.y) {
-		score++;
-		food = {
-			x: Math.floor(Math.random() * boxx * 58),
-			y: Math.floor(Math.random() * boxy * 18),
-		};
-  }
-  else
-  {
-     /* snak.pop() */
-    console.log("suprimer la valeur de x")
-  }
     }
-	
-  }
+   else {
+    alert ("merci de verifier votre saisir")
+   } 
 
-let game = setInterval(main, 300);
+}
+function suprimer()
+{
+    window.location.href="index.html"
+
+}
+// create box = 20 px ()
+const box = 20;
+// ajouter baground;
+const ground = new Image();
+ground.src = "image/Background.png";
+
+// random food
+const foodImg = new Image();
+let pices = ["image/kiwi.png","image/Fraise.png","image/ananas.png"];
+let randamimage = Math.floor(Math.random() * pices.length);
+console.log(randamimage);
+foodImg.src = pices[randamimage];
+
+// ajouter un sons a chaque action
+
+let more = new Audio();
+let mange = new Audio();
+let haut = new Audio();
+let droit = new Audio();
+let gauche = new Audio();
+let bas = new Audio();
+
+more.src ="sons/bomb.mp3";
+mange.src = "sons/mange.mp3";
+haut.src = "sons/click1.mp3";
+droit.src = "sons/click2.mp3";
+gauche.src = "sons/click3.mp3";
+bas.src = "sons/click4.mp3";
+
+// create the snake
+
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+// creation food sanck
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// creation le score et la direction de snack
+
+let score = 0;
+let s = 200;
+function scoree()
+{
+    if (score >= 6)
+      console.log("score" ,6);
+}
+console.log(s);
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        d = "LEFT";
+        gauche.play();
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+           haut.play();
+          
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+       droit.play();
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+         bas.play();
+    }
+    else if(key == 32 ){
+        console.log("Stop");
+        d="space";
+        ctx.fillText("snake stoped",cvs.width/2,cvs.height/2);
+    
+       
+    }
+    
+    
+    scoree()
+}
+// verifier si 1 des element de sanck touche  le hader function
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y && d!="space"){
+            return true;
+        }
+    }
+    return false;
+}
+
+// ajouter tes les fonction a l'element  canvas
+
+function draw(){
+      // ajouter un bar de cercle snak
+    ctx.drawImage(ground,0,0);
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(box, 3*box);
+    ctx.lineTo(ground.width,3*box);
+    ctx.stroke(); 
+    ctx.beginPath();
+    ctx.moveTo(box, 3*box);
+    ctx.lineTo(box,ground.height-3*box);
+    ctx.stroke(); 
+    ctx.beginPath();
+    ctx.moveTo(box, 3*box);
+    ctx.lineTo(box,ground.height -3*box);
+    ctx.stroke(); 
+    ctx.beginPath();
+    ctx.moveTo(ground.width-box, 3*box);
+    ctx.lineTo(ground.width-box,ground.height-3*box);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(box, ground.width-3*box);
+    ctx.lineTo(ground.width-box,ground.width - 3*box);
+    ctx.stroke(); 
+       // attribuer couleur au snak
+    for( let i = 0; i < snake.length ; i++){    
+        ctx.fillStyle = ( i == snake.length -1)? "yellow" : "white";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+        var img = new Image();
+       img.src = 'image/head.png';
+       img.width = 20;
+          var pattern = ctx.createPattern(img, 'repeat');
+        ctx.fillStyle = pattern;
+         ctx.fillRect(snake[0].x, snake[0].y, box, box);
+    }
+
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // position 0 de snak
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    // attribuer les cordonner de x et y
+    if( d == "LEFT") {snakeX -= box;}   
+    if( d == "UP") { snakeY -= box; }       
+    if( d == "RIGHT") {snakeX += box;}
+    if( d == "DOWN") {snakeY += box;}
+   
+    
+    
+    
+    
+    
+   
+    
+
+    // manger le food
+    if(snakeX == food.x && snakeY == food.y){
+     
+
+      inputscore.textContent=score;
+     
+    
+    
+      if (score > 10)
+      {    score++
+          console.log(s)
+      }
+      else 
+      {
+        score +=3
+      }
+      var gradient = ctx.createLinearGradient(0, 0, 200, 0);
+      mange.play();
+       gradient.addColorStop("0"," magenta");
+       gradient.addColorStop("0.5", "blue");
+       gradient.addColorStop("1.0", "red");
+       ctx.fillStyle = gradient;
+       ctx.fillText("catched!", food.x,food.y);
+        food = {
+            x : Math.floor(Math.random()*35+1) * box,
+            y : Math.floor(Math.random()*32+3) * box
+        }
+        // rien ne faire au tableux
+    }else{
+        // remove taille
+        snake.pop();
+    }
+    
+    // add new Head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+     //
+    // game over
+    let lastscore = localStorage.getItem("lastscore");
+    if(snakeX < box || snakeX > 36 * box  ||  snakeY < 3*box || snakeY > 34 * box || collision(newHead,snake)){
+        clearInterval(game);
+        more.play();
+ c.style = "display: block";
+ let div = document.createElement('div');
+ let h1 = document.createElement('h2');
+ let h2 = document.createElement('h2');
+  let span = document.createElement('span');
+ h1.innerText = "Game Over " + document.getElementById("nom").value + " Try Again";
+ h2.innerText = "Your Score is " + score;
+ div.appendChild(h1);
+ div.appendChild(h2);
+ div.appendChild(span);
+ c.appendChild(div); 
+ span.innerHTML = ' <button  onclick="suprimer(this)">paly again<button/>';
+ inputscore.style = "visibility : hidden";
+        console.log("fin");
+  ctx.font = "50px Tahoma";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillStyle = "White";
+  ctx.fillText("GAME OVER",cvs.width/2,cvs.height/2);
+
+  if (score > lastscore )
+  {
+  localStorage.setItem("lastscore", score);
+  localStorage.setItem("nom", document.getElementById("nom").value);
+  } 
+    }
+    
+    snake.unshift(newHead);
+    ctx.fillStyle="white";
+    ctx.font = "25px sans-serif";
+    ctx.fillText("score:",1*box,1.*box);
+    ctx.fillStyle="brown";
+    ctx.fillText(score,15*box,1.*box);
+    ctx.fillStyle="white";
+    ctx.fillText("Meilleur score",1*box,2.5*box);
+    ctx.fillStyle="brown";
+    ctx.fillText(lastscore,15*box,2.5*box);
+    ctx.fillStyle="white";
+    ctx.fillText("stop snak:space",20*box,1*box);
+    ctx.fillStyle="white";
+    ctx.fillText("Meilleur joueur:",20*box,2.5*box);
+    ctx.fillStyle="brown";
+    ctx.fillText(localStorage.nom,30*box,2.5*box);
+   
+}
+   
+
+
+game =setInterval(draw,s);
+  
+
+
+
+
+
+
+
+
+
+
+
+
